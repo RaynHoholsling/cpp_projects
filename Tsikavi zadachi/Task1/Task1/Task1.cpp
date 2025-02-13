94fc7edfd3d995f844a295efd9f9de98b6d501a7
RaynHoholsling <gns2008sng@gmail.com>
2025-02-13 20:45:14 +0200

    Big og commit

---
+++ Tsikavi zadachi/Task1/Task1/Task1.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+
+int main() {
+    int n;
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    for (int a = 1; a <= n; ++a) {
+        for (int b = a; b <= n; ++b) { // b >= a, щоб уникнути дублювання
+            int c = a * a + b * b;
+            int sqrt_c = static_cast<int>(sqrt(c));
+
+            if (sqrt_c * sqrt_c == c && sqrt_c <= n) {
+                std::cout << "(" << a << ", " << b << ", " << sqrt_c << ")\n";
+            }
+        }
+    }
+
+    return 0;
+}
+
