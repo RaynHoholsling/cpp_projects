62b648e060b246a8028f950323d7af7117ee42e7
RaynHoholsling <gns2008sng@gmail.com>
2025-02-17 22:47:04 +0200

    Revert "Delete Tsikavi zadachi/Task1 directory"
    This reverts commit 4372b2a7f15f331b241b3b1da13697d79e721f2e.

---
+++ Tsikavi zadachi/Task1/Task1/Task1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    for (int a = 1; a <= n; ++a) 
+    {
+        for (int b = a; b <= n; ++b) 
+        {
+            int c = a * a + b * b;
+            int sqrt_c = static_cast<int>(sqrt(c));
+
+            if (sqrt_c * sqrt_c == c && sqrt_c <= n) 
+            {
+                std::cout  << a << " " << b << " " << sqrt_c <<"\n";
+            }
+        }
+    }
+
+    return 0;
+}
+
