f3a9f7e57e2be41ffcf891041cfe9b5ca66a12f1
RaynHoholsling <gns2008sng@gmail.com>
2025-02-17 22:26:27 +0200

    task 4 (2)

--- Tsikavi zadachi/Task4/Task4/Task4.cpp
+++ Tsikavi zadachi/Task4/Task4/Task4.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-
+#include <chrono>
 
 int main() {
-    bool resheto[1001];
+    auto start_time = std::chrono::steady_clock::now();
 
-    for (int i = 0; i <= 1000; i++)
+    delete new int(1);
+    bool resheto[10001];
+
+    for (int i = 0; i <= 10000; i++)
     {
         resheto[i] = true;
     }
 
-    for (int p = 2; p * p <= 1000; p++)
+    for (int p = 2; p * p <= 10000; p++)
     {
         if (resheto[p]) {
-            for (int i = p * p; i <= 1000; i += p)
+            for (int i = p * p; i <= 10000; i += p)
             {
                 resheto[i] = false;
             }
         }
     }
 
-    for (int i = 2; i <= 1000; i++)
+    for (int i = 2; i <= 10000; i++)
     {
         if (resheto[i])
         {
             std::cout << " " << i ;
         }
     }
+    auto end_time = std::chrono::steady_clock::now();
+    auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+    std::cout <<"     " << elapsed_ns.count() << " ns\n";
     return 0;
 }
